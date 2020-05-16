0936780e7424a26881d3e742873b9d4e21ea0f6c
shubhankarb180 <shubhankarb180@gmail.com>
2020-05-16 20:42:14 +0530

    Some Folder Changes

--- src/App.js
+++ src/App.js
@@ -4,14 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = () => { 
-  return (
-    <div>
-      <h1>Hats Page</h1>
-    </div>
-  );
-}
+import ShopPage from './pages/shop/shop.component';
 
 class App extends React.Component { 
   render() { 
@@ -19,7 +12,7 @@ class App extends React.Component {
       <div>
         <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route path='/hats' component={HatsPage} />
+          <Route path='/shop' component={ShopPage} />
         </Switch>
       </div>
     );
