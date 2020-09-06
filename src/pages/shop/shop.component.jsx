40c2a5f9de02d2795b93277b323c0517c5bdc340
shubhankarb180 <shubhankarb180@gmail.com>
2020-09-06 15:06:24 +0530

    Feat: Shop Page, Collection Preview and Collection Item component Added

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import SHOP_DATA from './shop.data';
 
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+
 class ShopPage extends React.Component { 
     constructor(props) {
         super(props);
@@ -12,8 +14,16 @@ class ShopPage extends React.Component {
     }
 
     render() {
+        const {collections} = this.state;
         return (
-            <div> Shop Page</div>
+            <div className="shop-page">
+                {
+                    collections.map(({id, ...otherCollectionProps}) => (
+                            <CollectionPreview key={id} {...otherCollectionProps} />
+                        )
+                    )
+                }
+            </div>
         );
     }
 }
